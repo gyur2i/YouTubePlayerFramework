20ee4f063134c5f8ae62aecca444e6dc3f5312b8
gyur2i <kyuri0821@naver.com>
2022-02-18 13:46:52 +0900

    Initial Commit

---
+++ YouTubePlayerFramework/YouTubePlayerFramework.h
@@ -0,0 +1,18 @@
+//
+//  YouTubePlayerFramework.h
+//  YouTubePlayerFramework
+//
+//  Created by 김규리 on 2022/02/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YouTubePlayerFramework.
+FOUNDATION_EXPORT double YouTubePlayerFrameworkVersionNumber;
+
+//! Project version string for YouTubePlayerFramework.
+FOUNDATION_EXPORT const unsigned char YouTubePlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouTubePlayerFramework/PublicHeader.h>
+
+
